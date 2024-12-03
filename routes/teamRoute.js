92cf54ded852842862e1ae3387605d1eae003f45
Nantawat <nantawattanasarn@gmail.com>
2024-12-03 16:49:22 +0700

    first commit mongodbminiproject

---
+++ routes/teamRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getTeamMembersByManager } from "../controllers/teamController.js";
+
+const teamRouter = express.Router();
+
+teamRouter.get("/:manager_id", getTeamMembersByManager);
+
+export default teamRouter;
